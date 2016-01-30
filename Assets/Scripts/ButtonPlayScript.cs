9f6cb3c53c5aa1d20f35ccb6bf250f497b49ace2
Vinny <eirbcocorico@gmail.com>
2016-01-30 02:01:48 +0100

    Commit Menu V0 and health bar V0

---
+++ Assets/Scripts/ButtonPlayScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonPlayScript : MonoBehaviour {
+
+	public int m_levelNumber;
+	public AudioSource m_audio;
+	bool clicked = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (clicked && !m_audio.isPlaying) 
+		{
+			Application.LoadLevel (m_levelNumber);
+		}
+	}
+
+	public void OnClick(){
+		m_audio.Play ();
+		clicked = true;
+
+	}
+}
